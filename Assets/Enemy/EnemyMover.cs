41664155aaa725e0c3da5df8b99c58ebe0b50215
Tenusia <tenusia@gmail.com>
2023-02-13 17:27:42 +0900

    add enemy prefab. Add enemy movement and path tracking. Add enemy health system. Add mouse control. Add tower placement on grid. Add fire sequence and damaging for defence towers.

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     void Start()
     {
-        PrintWaypointName();
+        StartCoroutine(FollowPath());
     }
 
-    void PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition,endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();           
+            }
+            
         }
     }
 }
